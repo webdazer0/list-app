095ef46c6c1de63155776bf17f31dbd1ffcbd7f1
MigueL C <webdazer0@gmail.com>
2024-01-05 21:45:15 +0100

    chore: improve task card (avatar & lastUpdate) & button add Task

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { apiService } from '../services/api.service';
 
+import { format } from 'date-fns';
+
 export default function TaskList() {
   const [tasks, setTasks] = useState([]);
 
@@ -26,8 +28,19 @@ export default function TaskList() {
 
   return (
     <div className="container">
-      <h1>Task List</h1>
-      <div className="row">
+      <div className="d-flex justify-content-between py-4 align-items-center">
+        <h4 className="mb-0">
+          Tasks
+          <span className="badge badge-danger rounded-circle ml-2 small h6">
+            {tasks.length}
+          </span>
+        </h4>
+        <Link className="btn alert-success" to="/task/create">
+          <i className="fa fa-add pr-1" aria-hidden="true"></i> Add Task
+        </Link>
+      </div>
+
+      <div className="row row-gap">
         <ItemTaskList items={tasks} onDelete={deleteTask} />
       </div>
     </div>
@@ -43,29 +56,86 @@ function ItemTaskList(props) {
 }
 
 function ItemTask({ task, onDelete }) {
+  const updatedAt = format(new Date(task.updatedAt), 'dd MMM, yyyy');
+
   return (
-    <div className="col-md-4">
-      <div className="card">
-        <div className="card-header d-flex justify-content-between">
-          {task.username}{' '}
-          <span className="badge badge-pill badge-success">
-            {task.duration}
-          </span>
+    <div className="col-lg-4 col-md-6 col-sm-12">
+      <div className="card rounded-lg shadow-sm ">
+        <div className="card-header d-flex justify-content-between pr-0">
+          {task.username}
+          <div className="btn-group">
+            <button
+              type="button"
+              className="btn py-0"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+            </button>
+            <div className="dropdown-menu">
+              <Link to={'/task/edit/' + task._id} className="dropdown-item">
+                <i
+                  className="fa fa-pencil text-warning pr-2"
+                  aria-hidden="true"
+                ></i>
+                Edit
+              </Link>
+              <Link
+                to="/"
+                onClick={() => onDelete(task._id)}
+                className="dropdown-item"
+              >
+                <i
+                  className="fa fa-trash text-danger pr-2"
+                  aria-hidden="true"
+                ></i>
+                Delete
+              </Link>
+              <div className="dropdown-divider"></div>
+              <div className="dropdown-item">View Author</div>
+            </div>
+          </div>
         </div>
         <div className="card-body">
-          <p>{task.description}</p>
+          <p className="custom-wrap">
+            <b>{task.description}</b>
+          </p>
+          <span className="badge badge-pill badge-info">{task.duration}</span>
         </div>
-        <div className="card-footer">
-          <Link to={'/edit/' + task._id} className="btn btn-success mr-2">
-            Edit
-          </Link>
-          <Link
-            to="/"
-            onClick={() => onDelete(task._id)}
-            className="btn btn-danger"
-          >
-            Delete
-          </Link>
+        <div className="card-body pt-0">
+          {task.duration === 2005 && (
+            <span className=" alert alert-success py-1 px-2 small">
+              Completed
+              <i className="fa fa-check  pl-1" aria-hidden="true"></i>
+            </span>
+          )}
+        </div>
+        <div className="card-footer d-flex justify-content-between align-items-center">
+          {/* <div>
+            <Link to={'/edit/' + task._id} className="btn btn-success mr-2">
+              <i className="fa fa-pencil" aria-hidden="true"></i>
+            </Link>
+            <Link
+              to="/"
+              onClick={() => onDelete(task._id)}
+              className="btn btn-danger"
+            >
+              <i className="fa fa-trash" aria-hidden="true"></i>
+            </Link>
+          </div> */}
+          <span className="small">
+            Last updated: <b>{updatedAt}</b>
+          </span>
+          {/* <span>Last updated: 10 Dec, 2022</span> */}
+
+          <span
+            className="avatar bg-secondary text-white"
+            data-name={task.username?.slice(0, 2)?.toUpperCase()}
+            data-toggle="tooltip"
+            data-placement="top"
+            title={task.username}
+          ></span>
         </div>
       </div>
     </div>
