6c63eb4bbe014a853a17d346c6f20931f5f34318
MigueL C <webdazer0@gmail.com>
2024-01-07 23:55:58 +0100

    refactor: update `CreateTask` & `EditTask` with new task model

--- frontend/src/components/CreateTask.jsx
+++ frontend/src/components/CreateTask.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { apiService } from '../services/api.service';
-import { DropdownField, TextAreaField, TextField } from './form';
+import { DateField, DropdownField, TextAreaField, TextField } from './form';
 import useUsers from '../hooks/useUsers';
 import Button from './ui/Button';
 import { useFieldPro } from '../hooks/useFieldPro';
@@ -10,6 +10,7 @@ const emptyTask = {
   username: '',
   description: '',
   tags: '',
+  date: '',
 };
 
 export default function CreateTask() {
@@ -24,7 +25,8 @@ export default function CreateTask() {
   const onSubmit = (event) => {
     event.preventDefault();
     const tags = data.tags.split(',').map((tag) => tag.trim());
-    onSaveTask({ ...data, tags });
+    const date = new Date(data.date);
+    onSaveTask({ ...data, tags, date });
   };
 
   const onSaveTask = (task) => {
@@ -38,24 +40,25 @@ export default function CreateTask() {
   const required = { required: true };
 
   useEffect(() => {
-    console.log('✅ useEffect CT');
     if (users.length <= 0) return;
     addPartialData({ username: users[0] });
   }, [users, addPartialData]);
 
   return (
     <>
-      <div className="row py-4">
-        <div className="col-md-2">
+      <div className="row pt-4 pb-3 align-items-center">
+        <div className="col-md-3">
           <button className="btn alert-success" onClick={goBack}>
             <i className="fa fa-arrow-left pr-1" aria-hidden="true"></i>back
           </button>
         </div>
+        <div className="col-md-6">
+          <div className="h4 mb-0">Create New Task</div>
+        </div>
       </div>
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <div className="card my-4">
-            <div className="card-header">Create Task</div>
+          <div className="card mt-0 mb-4">
             <div className="card-body">
               <form onSubmit={onSubmit}>
                 <DropdownField
@@ -64,6 +67,7 @@ export default function CreateTask() {
                 />
                 <TextAreaField {...register('description', required)} />
                 <TextField {...register('tags', required)} />
+                <DateField {...register('date', required)} />
                 <Button variant="primary">Save</Button>
               </form>
             </div>
--- frontend/src/components/EditTask.jsx
+++ frontend/src/components/EditTask.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { apiService } from '../services/api.service';
-import { DropdownField, TextAreaField, TextField } from './form';
+import { DateField, DropdownField, TextAreaField, TextField } from './form';
 import Button from './ui/Button';
 import { useTask } from '../hooks/useTask';
 import useUsers from '../hooks/useUsers';
 import { useFieldPro } from '../hooks/useFieldPro';
+import { format } from 'date-fns';
+import { CheckboxField } from './form/CheckboxField';
 
 const emptyTask = {
   username: '',
   description: '',
   tags: '',
+  date: '',
+  done: false,
 };
 
 export default function EditTask() {
@@ -27,7 +31,8 @@ export default function EditTask() {
   const onSubmit = (event) => {
     event.preventDefault();
     const tags = data.tags.split(',').map((tag) => tag.trim());
-    onUpdateTask({ ...data, tags });
+    const date = new Date(data.date);
+    onUpdateTask({ ...data, tags, date });
   };
 
   const onUpdateTask = (task) => {
@@ -38,36 +43,46 @@ export default function EditTask() {
       .finally(() => goToHomePage());
   };
 
+  const required = { required: true };
+
   useEffect(() => {
     if (!task._id) return;
-    const newTask = { ...task, tags: task.tags?.join(',') ?? '' };
+    const date = format(new Date(task.date), 'yyyy-MM-dd');
+    const newTask = { ...task, tags: task.tags?.join(',') ?? '', date };
     console.log({ newTask });
     addPartialData(newTask);
   }, [task, addPartialData]);
 
   return (
     <>
-      <div className="row py-4">
-        <div className="col-md-2">
+      <div className="row pt-4 pb-3 align-items-center">
+        <div className="col-md-3">
           <button className="btn alert-success" onClick={goBack}>
             <i className="fa fa-arrow-left pr-1" aria-hidden="true"></i>back
           </button>
         </div>
+        <div className="col-md-6">
+          <div className="h4 mb-0">Edit Task</div>
+        </div>
       </div>
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <div className="card my-4">
-            <div className="card-header">EDIT Task</div>
-            <div className="card-body">
-              <form onSubmit={onSubmit}>
-                <DropdownField {...register('username')} items={users} />
-                <TextAreaField {...register('description')} />
-                <TextField {...register('tags')} />
+          <form onSubmit={onSubmit}>
+            <div className="card mt-0 mb-4">
+              <div className="card-body">
+                <DropdownField
+                  {...register('username', required)}
+                  items={users}
+                />
+                <TextAreaField {...register('description', required)} />
+                <TextField {...register('tags', required)} />
+                <DateField {...register('date', required)} />
+                <CheckboxField {...register('done')} label="complete" />
                 <div className="pb-4"></div>
-                <Button variant="primary">Update</Button>
-              </form>
+                <Button variant="warning">Update</Button>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
